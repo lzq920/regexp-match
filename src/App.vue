ef7469e75b0dfa16ab3993356e0efa487c0ec1b1
eleven <lizhiqiang@meb.com>
2021-04-26 09:58:27 +0800

    添加常用正则测试用例

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <h1 class="title">Regexp Match</h1>
     <div class="reg">
       <span>/</span>
       <input type="text" v-model.trim="reg" class="reg-input" maxlength="50" @input="handleChange">
@@ -11,22 +12,30 @@
       <span class="match-tag" v-for="(item,index) in matchs" :key="index" :style="item"></span>
     </div>
     <div class="tip">
-      <div class="tip-item" v-for="(item,index) in tipList" :key="index" @click="appendReg(item.reg)">
+      <div class="tip-title">常用规则</div>
+      <div class="tip-item" v-for="(item,index) in tipList" :key="index">
         {{ item.reg }}
         <div class="tip-item-hover">{{ item.tip }}</div>
+      </div>
+    </div>
+    <div class="tip">
+      <div class="tip-title">测试用例</div>
+      <div class="tip-item" v-for="(item,index) in testList" :key="index" @click="handleClick(item)">
+        {{ item.tip }}
+        <div class="tip-item-hover">{{ item.reg }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue'
+import {ref, reactive, onMounted} from 'vue'
 
 const reg = ref('eleven')
 const flags = ref('g')
 const matchs = ref([])
 const text = ref('eleven2333')
-const tipList = ref([{
+const tipList = reactive([{
   reg: '.',
   tip: '匹配除换行符以外的任意字符'
 }, {
@@ -93,6 +102,48 @@ const tipList = ref([{
   reg: '[!a-z]',
   tip: '匹配任何不在指定范围内的任意字符'
 }]);
+const testList = reactive([
+  {
+    tip: '手机号码',
+    reg: '^1[3-9]\\d{9}$',
+    test: '13333333333'
+  },
+  {
+    tip: '英文和数字',
+    reg: '^[0-9a-zA-Z]+$',
+    test: '123123123ASDFASDFafsdf',
+  },
+  {
+    tip: '电话号码',
+    reg: '^\\d{3}-\\d{8}|\\d{4}-\\d{7}$',
+    test: '021-88888888'
+  },
+  {
+    tip: '日期格式',
+    reg: '^\\d{4}-\\d{1,2}-\\d{1,2}',
+    test: '2021-04-26'
+  },
+  {
+    tip: 'HTML标签',
+    reg: '<(\\S*?)[^>]*>.*?|<.*? />',
+    test: '<p>123</p>'
+  },
+  {
+    tip: '版本号格式必须为X.Y.Z',
+    reg: '^\\d+(?:\\.\\d+){2}$',
+    test: '1.0.0'
+  },
+  {
+    tip: '银行卡号',
+    reg: '^[1-9]\\d{9,29}$',
+    test: '6222026006705354000'
+  },
+  {
+    tip: '微信号',
+    reg: '^[a-zA-Z][-_a-zA-Z0-9]{5,19}$',
+    test: 'a23123'
+  }
+])
 const throttle = (fn, wait) => {
   let inThrottle, lastFn, lastTime;
   return function () {
@@ -163,8 +214,10 @@ const matchRegexp = () => {
 const handleChange = throttle(() => {
   matchRegexp()
 }, 1000)
-const appendReg = (str) => {
-  reg.value += str;
+const handleClick = (obj) => {
+  reg.value = obj.reg;
+  text.value = obj.test;
+  matchRegexp()
 }
 onMounted(() => {
   matchRegexp()
@@ -195,6 +248,11 @@ html, body {
   display: flex;
   flex-direction: column;
   justify-content: center;
+}
+
+.title {
+  text-align: center;
+  color: #FFCB16
 }
 
 .reg {
@@ -263,10 +321,19 @@ html, body {
 }
 
 .tip {
+  position: relative;
   margin-top: 100px;
   display: flex;
   flex-wrap: wrap;
   gap: 0.2rem;
+}
+
+.tip .tip-title {
+  position: absolute;
+  font-size: 18px;
+  left: 0;
+  top: -30px;
+  color: #ffffff;
 }
 
 .tip .tip-item {
@@ -280,7 +347,7 @@ html, body {
 }
 
 .tip .tip-item:hover {
-  background-color: #9C27B0;
+  background-color: #17522E;
 }
 
 .tip .tip-item .tip-item-hover {
@@ -289,11 +356,15 @@ html, body {
   left: 0;
   top: -200%;
   white-space: nowrap;
-  background: #9C27B0;
+  background: #17522E;
   color: white;
   padding: 16px;
   font-size: 16px;
   transition: all 300ms;
+}
+
+.tip .tip-item:hover {
+  border: 1px solid #17522E;
 }
 
 .tip .tip-item:hover > .tip-item-hover {
